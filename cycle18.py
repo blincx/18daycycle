0e22f313002fc2f7fce0bc0d31b88d7d230d5eeb
Blincow, Eric <eblincow@RCHSD.org>
2020-03-06 11:43:21 -0800

    final fully working

--- cycle18.py
+++ cycle18.py
@@ -13,7 +13,6 @@ def earliest_possible_first_day():
 def cycle_up(first_date,multiple):
     new_first_date = first_date + timedelta(days=(18*multiple))
     # nota bene: go one day beyond, to start the new cycle
-    print(repr(new_first_date))
     return new_first_date
 
 # 2/26/2020 - recent first day
@@ -22,24 +21,25 @@ fast_forward_to_recent_first_day = cycle_up(StartDate,721) # already experienced
 # days from first day of 722 cycle to now
 num_of_days_to_label = (datetime.now() - fast_forward_to_recent_first_day).days + 1 # + 1 because it needs to round up
 
-print(fast_forward_to_recent_first_day)
-print(num_of_days_to_label)
 
 day_collector = []
 start_loop = fast_forward_to_recent_first_day
 day_collector.append(("DATE","DAY IN CYCLE"))
 counter = 1
-day_collector.append((start_loop.strftime("%d/%m/%y"), counter))
+day_collector.append((start_loop.strftime("%d/%m"), counter))
 for dayindex in range(1,num_of_days_to_label):
+    if counter==18: counter = 0 # reset
     counter += 1
     new_day = start_loop + timedelta(days=1)
     start_loop = new_day
-    day_collector.append((new_day.strftime("%d/%m/%y"),counter))
-#d.strftime("%d/%m/%y")
-pprint.pprint(day_collector)
+    day_collector.append((new_day.strftime("%d/%m"),counter))
+    todays_value_in_cycle = counter
 
-
-
-
+prettybar = "=========================="
+print(f"\n{prettybar}")
+print(f"DAY - DAY IN CYCLE\n{prettybar}")
+for day,day_in_cyc in day_collector[-10:]:
+    print(f"{day} - {day_in_cyc}")
+print(f"{prettybar}")
 
 
