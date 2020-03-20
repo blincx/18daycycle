c12e258a8629c359e5fbd61e5793130ea007f64d
Eric Blincow <eblincow@barracuda.com>
2020-03-20 03:54:20 -0700

    I have a useable today variable

--- cycle_result.php
+++ cycle_result.php
@@ -7,19 +7,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo $birthdate;
     $command = escapeshellcmd($cwd . '/' . 'cycle18.py ' . $birthdate . ' json');
     $output = shell_exec($command);
-    echo $output;
+    // echo $output;
 }
 
 ?>
 
-<canvas id="myCanvas" width="360" height="360" style="border:1px solid #d3d3d3;">
-
+<canvas id="myCanvas" width="360" height="360" style="border:1px solid #d3d3d3; z-index: 1;" />
+<canvas id="background" width="360" height="360" style="z-index: 2;" />
 
 <script>
+var today = "<?php echo $today ?>";
+//var data = JSON.parse(serverdata);
+
+var data = <?=$output?>;
+var today = data['today'];
+
+console.log(today);
+
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var i;
 for(i=0; i<360; i+= 20){
+    if ((i/20)==today) {
+        console.log("TODAY IS THE GREATEST!");
+
+    }
     ctx.moveTo(i+5,180);
     ctx.lineTo(i,180);
 }
