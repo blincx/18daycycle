b7a6039bbc14c49fe61957d77afd442bb4a6f3de
Eric Blincow <eblincow@barracuda.com>
2020-03-20 20:42:24 -0700

    working modals

--- cycle_result.php
+++ cycle_result.php
@@ -44,7 +44,7 @@
             <div id="sinewavecontainer" class="wrapper"></div>
     </div>
     <div id="second_container" class="container2">
-       <button class="button blue">See Extended Schedule</button>
+       <button class="button blue" id="schedule_button">See Extended Schedule</button>
     </div>
 </div>
 
@@ -70,6 +70,60 @@ var which_img_to_show = "pics/cyc_".concat(today).concat(".jpg");
 window.onload = show_image(which_img_to_show, "cycle_sine");
 
 </script>
+
+
+
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2 style="text-align: center;">&#9765;</h2>
+    </div>
+    <div class="modal-body">
+      <p>Some text in the Modal Body</p>
+      <p>Some other text...</p>
+    </div>
+    <div class="modal-footer">
+        <br/>
+    </div>
+  </div>
+
+</div>
+
+
+<script>
+// MODAL STUFF 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("schedule_button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
 
 
 </body>
