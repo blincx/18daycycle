a352c39d0e1e1d53f3d6ab160d1a29b5c60c1c08
Eric Blincow <eblincow@barracuda.com>
2020-03-22 13:42:27 -0700

    got modula method... working

--- confirm18.py
+++ confirm18.py
@@ -51,30 +51,30 @@ def test_modulo(birthday,today):
             found_last_18th_day = True
             last_18th_day = copy.copy(loopy_day)
         else:
-        # go back another day
-        loopy_day = loopy_day - timedelta(days=1)
+            # go back another day
+            loopy_day = loopy_day - timedelta(days=1)
     
-    # label the days
 
+    last_1st_day = last_18th_day + timedelta(days=1)
     did_we_reach_today = False
-    
-    cycle_day_counter = 18 #remember, starting with 18th day
+    another_loopy_day = copy.copy(last_1st_day)
+    CURRENT_CYCLE_DAY = 1
     # now cycle forward till we hit today
     while (did_we_reach_today==False):
-        
+        if (another_loopy_day==today):        
+            did_we_reach_today = True
+            return CURRENT_CYCLE_DAY
+        elif (CURRENT_CYCLE_DAY==18):
+            raise "You broke me. Get on my level."
+        else:
+            # increment day
+            another_loopy_day = another_loopy_day + timedelta(days=1)
+            # increment CURRENT_CYCLE_DAY
+            CURRENT_CYCLE_DAY = CURRENT_CYCLE_DAY + 1
+
+    raise "Shouldn't have gotten here. Somethings broken."
 
 
-
-
-    print(f"today = {today}\nbirthday={birthday}\nnumber_of_days_between={totaldays}")
-    modulo = totaldays%18
-    print(f"modulo={modulo}")
-    daydiff_until_last_day_18 = 18 - modulo
-    # last 18 day
-    newdate = today - timedelta(days=daydiff_until_last_day_18)
-    # number of days between last 18 day and birthday
-    newdiff = (newdate - birthday).days
-    print(f"daydiff_until_last_day_18={daydiff_until_last_day_18};\nnewdate={newdate}\nnewdiff={newdiff}\n");
     assert(newdiff ==(today - birthday).days - daydiff_until_last_day_18);
     new_modulo = newdiff%18
     print(new_modulo)
@@ -86,7 +86,7 @@ def test_modulo(birthday,today):
 
 def main():
     birthday,today,totaldays = setup()
-    test = test_modulo(birthday,today,totaldays)
+    test = test_modulo(birthday,today)
     print(test)
     #current_day_position = get_current_day_position_in_cycle_modulo_method(birthday,today,totaldays)
     #print(current_day_position)
